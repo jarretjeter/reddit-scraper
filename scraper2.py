a15ffdb9bac75d03707ac23a5281445ed5c6446c
pooh <jarretjeter@gmail.com>
2022-11-01 23:03:17 -0700

    save csv's in batches of 500 rows to save memory, change naming of csv's

--- scraper2.py
+++ scraper2.py
@@ -1,7 +1,8 @@
-from datetime import datetime
+import datetime as dt
 import logging
 from logging import INFO
 import os
+from time import sleep, time
 import pandas as pd
 import praw
 from psaw import PushshiftAPI
@@ -33,29 +34,46 @@ def fetch_threads(subreddit: str, query: str, limit=None, csv=None) -> pd.DataFr
     threads = api.search_submissions(
         subreddit=subreddit,
         q=query,
-        after=int(datetime(2012, 1, 1).timestamp()) - 1,
-        before=int(datetime.now().timestamp()),
+        after=int(dt.datetime(2012, 1, 1).timestamp()) - 1,
+        before=int(dt.datetime.now().timestamp()),
         limit=limit
     )
     meta_list = [thread.__dict__ for thread in threads]
 
     for meta_dict in meta_list:
-        data_dict["ID"] += [meta_dict["id"]]
-        data_dict["Title"] += [meta_dict["title"]]
-        data_dict["Subreddit"] += [meta_dict["subreddit_name_prefixed"]]
-        data_dict["Date"] += [datetime.fromtimestamp(meta_dict["created_utc"])]
-        data_dict["Author"] += [meta_dict["author"]]
-        data_dict["Upvotes"] += [meta_dict["ups"]]
-        data_dict["Ratio"] += [meta_dict["upvote_ratio"]]
-        data_dict["Num_Comments"] += [meta_dict["num_comments"]]
-        data_dict["URL"] += [f"https://www.reddit.com{meta_dict['permalink']}"]
-
+        if len(data_dict["ID"]) < 500:
+            data_dict["ID"] += [meta_dict["id"]]
+            data_dict["Title"] += [meta_dict["title"]]
+            data_dict["Subreddit"] += [meta_dict["subreddit_name_prefixed"]]
+            data_dict["Date"] += [dt.datetime.fromtimestamp(meta_dict["created_utc"])]
+            data_dict["Author"] += [meta_dict["author"]]
+            data_dict["Upvotes"] += [meta_dict["ups"]]
+            data_dict["Ratio"] += [meta_dict["upvote_ratio"]]
+            data_dict["Num_Comments"] += [meta_dict["num_comments"]]
+            data_dict["URL"] += [f"https://www.reddit.com{meta_dict['permalink']}"]
+            logger.info("fetching next thread")
+        else:
+            df = pd.DataFrame(data_dict)
+            if csv:
+                logger.info("Batch size 500 reached. Saving current batch to csv file")
+                query = query.replace(" ", "_")
+                df["Author"].fillna("[deleted]", inplace=True)
+                df["Date"] = pd.to_datetime(df["Date"])
+                min_date = min(df['Date'].dt.date)
+                max_date = max(df['Date'].dt.date)
+                df.to_csv(f"./data/{subreddit}_{query}_threads-{min_date}-{max_date}.csv", index=False)
+            # Reset data dictionary to save memory
+            data_dict = {"ID": [], "Title" : [], "Subreddit": [], "Date": [], "Author": [], "Upvotes": [], "Ratio": [], "Num_Comments": [], "URL": []}
         
     df = pd.DataFrame(data_dict)
     if csv:
-        logger.info("Saving to csv file")
+        logger.info("Saving final batch to csv file")
         query = query.replace(" ", "_")
-        df.to_csv(f"./data/{query}_threads.csv", index=False)
+        df["Author"].fillna("[deleted]", inplace=True)
+        df["Date"] = pd.to_datetime(df["Date"])
+        min_date = min(df['Date'].dt.date)
+        max_date = max(df['Date'].dt.date)
+        df.to_csv(f"./data/{subreddit}_{query}_threads-{min_date}-{max_date}.csv", index=False)
     return df
 
 
@@ -69,60 +87,64 @@ def fetch_comments(subreddit, query, limit=None, csv=None):
     comments = list(api.search_comments(
         subreddit=subreddit,
         q=query,
-        after=int(datetime(2012, 1, 1).timestamp()) - 1,
-        before=int(datetime.now().timestamp()),
+        after=int(dt.datetime(2018, 1, 1).timestamp()) - 1,
+        before=int(dt.datetime(2022, 10, 7).timestamp()),
         limit=limit
     ))
 
-    current_selftext = ""
     past_url = ""
 
     for comment in comments:
-        data_dict["ID"] += [f"t1_{comment.id}"]
-        # metadata_object = reddit.info(fullnames=[comment.link_id])
-        # for item in metadata_object:
-        #     data_dict["Thread_Title"] += [item.title]
-        # thread_object = metadata_object.submission
-        # print(thread_object.selftext)
-        # print(comment.submission)
-        current_submission = comment.submission
-        data_dict["Thread_Title"] += [current_submission.title]
-        url = f"https://reddit.com{comment.permalink}"
-        submission_id = re.search("(comments)\/\w*\/", url).group()
-        if submission_id != past_url and current_submission.selftext != "" and current_submission.selftext != current_selftext:
+        if len(data_dict["ID"]) < 500:
+            data_dict["ID"] += [f"t1_{comment.id}"]
+            current_submission = comment.submission
+            data_dict["Thread_Title"] += [current_submission.title]
+            url = f"https://reddit.com{comment.permalink}"
+            submission_id = re.search("(comments)\/\w*\/", url).group()
+            # Only need the selftext once per thread
+            if submission_id != past_url and current_submission.selftext != "":
                 current_submission.selftext = current_submission.selftext.replace("\n", "").replace("\r", "")
                 data_dict["Comment"] += [current_submission.selftext]
-                data_dict["Date"] += [datetime.fromtimestamp(int(comment.created_utc))]
+                data_dict["Date"] += [dt.datetime.fromtimestamp(int(comment.created_utc))]
                 data_dict["Author"] += [comment.author]
                 data_dict["Upvotes"] += [comment.score]
                 data_dict["Subreddit"] += [comment.subreddit]
                 data_dict["URL"] += [url]
-                current_selftext = current_submission.selftext
-        # if current_submission.selftext != "" and current_submission.selftext != current_selftext:
-        #     current_submission.selftext = current_submission.selftext.replace("\n", "").replace("\r", "")
-        #     data_dict["Comment"] += [current_submission.selftext]
-        #     data_dict["Date"] += [datetime.fromtimestamp(int(comment.created_utc))]
-        #     data_dict["Author"] += [comment.author]
-        #     data_dict["Upvotes"] += [comment.score]
-        #     data_dict["Subreddit"] += [comment.subreddit]
-        #     data_dict["URL"] += [f"https://reddit.com{comment.permalink}"]
-        #     current_selftext = current_submission.selftext
+                sleep(1.0)
+                logger.info("Fetching next comment")
 
+            else:
+                comment.body = comment.body.replace("\n", "").replace("\r", "")
+                data_dict["Comment"] += [comment.body]
+                data_dict["Date"] += [dt.datetime.fromtimestamp(int(comment.created_utc))]
+                data_dict["Author"] += [comment.author]
+                data_dict["Upvotes"] += [comment.score]
+                data_dict["Subreddit"] += [comment.subreddit]
+                data_dict["URL"] += [f"https://reddit.com{comment.permalink}"]
+                sleep(1.0)
+                logger.info("Fetching next comment")
         else:
-            comment.body = comment.body.replace("\n", "").replace("\r", "")
-            data_dict["Comment"] += [comment.body]
-            data_dict["Date"] += [datetime.fromtimestamp(int(comment.created_utc))]
-            data_dict["Author"] += [comment.author]
-            data_dict["Upvotes"] += [comment.score]
-            data_dict["Subreddit"] += [comment.subreddit]
-            data_dict["URL"] += [f"https://reddit.com{comment.permalink}"]
-            logger.info("Fetching next comment")
+            df = pd.DataFrame(data_dict)
+            if csv:
+                logger.info("Batch size 500 reached. Saving current batch to csv file")
+                query = query.replace(" ", "_")
+                df["Author"].fillna("[deleted]", inplace=True)
+                df["Date"] = pd.to_datetime(df["Date"])
+                min_date = min(df['Date'].dt.date)
+                max_date = max(df['Date'].dt.date)
+                df.to_csv(f"./data/{subreddit}_{query}_comments-{min_date}-{max_date}.csv", index=False)
+            # Reset data dictionary to save memory
+            data_dict = {"ID": [], "Thread_Title": [], "Comment": [], "Date": [], "Author": [], "Upvotes": [], "Subreddit": [], "URL": []}
 
     df = pd.DataFrame(data_dict)
     if csv:
-        logger.info("Saving to csv file")
+        logger.info("Saving final batch to csv file")
         query = query.replace(" ", "_")
-        df.to_csv(f"./data/{query}_comments.csv", index=False)
+        df["Author"].fillna("[deleted]", inplace=True)
+        df["Date"] = pd.to_datetime(df["Date"])
+        min_date = min(df['Date'].dt.date)
+        max_date = max(df['Date'].dt.date)
+        df.to_csv(f"./data/{subreddit}_{query}_comments-{min_date}-{max_date}.csv", index=False)
     return df
 
 
